3057149b5655de376eec12a0f5c8559beca8f8cb
Jasmine W <jazzi93@gmail.com>
2019-02-12 12:16:46 -0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-auto py-3" style="bottom: 0; position: absolute; width: 100%;">
+    <span>Built with &hearts; by <a href="https://jasminew.xyz">Jasmine</a>. Cat illustration by <a href="https://www.freepik.com/free-photos-vectors/hand">terdpongvector</a>.</span>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?> 
+    </body>
+    </html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php 
+
+// Advanced Custom Fields
+$cat_image	=	get_field('cat_image');
+$main_title	=	get_field('main_title');
+$subtitle	=	get_field('subtitle');
+
+get_header(); ?>
+
+	<div class="top">
+        <h1><?php echo $main_title; ?></h1>
+        <?php if (!empty($subtitle)) : ?><h2><?php echo $subtitle; ?></h2>
+		<?php endif; ?>
+            <!-- if user uploaded image -->
+		
+		<?php if ( !empty($cat_image)) : ?><img src="<?php echo $cat_image; ?>" class="main-img img-fluid"/>
+		<?php endif; ?>
+        </div>
+        
+        <div class="container">
+
+       <div class="info">
+
+           <div class="row">
+			   
+			   <!-- features -->
+			   <?php $loop = new WP_Query( array( 'post_type' => 'features', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+			   
+			   <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+			   
+			   <div class="col-md-3"><i class="<?php the_field('feature_icon'); ?>"></i><p><?php the_title(); ?></p></div>
+			   
+			   <?php endwhile; ?>
+				<!-- end features -->
+
+				   </div>
+            </div>
+            
+            <h3 style="text-align: center;">Other people think Oliver is purrfect, too.</h3>
+			
+			<!-- testimonials -->
+			
+           <div class="testimonial">
+               <div class="row">
+				   
+				   <?php $loop = new WP_Query( array( 'post_type' => 'testimonial', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+			   
+			   <?php while( $loop->have_posts() ) : $loop->the_post(); ?>
+				   
+				   <div class="col-md-4 d-flex">
+                   <div class="card">
+                       <div class="card-body flex-fill">
+                        <blockquote class="blockquote mb-0">
+						  <p><?php the_content(); ?></p>
+						  <footer class="blockquote-footer"><?php the_title(); ?></footer>
+    </blockquote>
+                       </div>
+                    </div>
+                   </div>
+				   
+				   <?php endwhile; ?>
+				   
+				   
+           
+                    </div>
+               </div>
+			<!-- end testimonials -->
+			
+           </div>
+
+
+<?php get_footer(); ?>
