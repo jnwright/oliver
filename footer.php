63292995a55eaa9f5bda557343830c204172d7ed
Jasmine W <jazzi93@gmail.com>
2019-02-12 12:31:45 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="mt-auto py-3" style="bottom: 0; position: absolute; width: 100%;">
+<footer class="mt-auto py-3" style="bottom: 0; position: fixed; width: 100%; text-align: center;">
     <span>Built with &hearts; by <a href="https://jasminew.xyz">Jasmine</a>. Cat illustration by <a href="https://www.freepik.com/free-photos-vectors/hand">terdpongvector</a>.</span>
     </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
